d8c7ebfe5a8da1ad029414398d1feb34c5d5e036
Arthur Plazanet <plazanet.art@gmail.com>
2025-03-01 02:10:58 +0100

    Setup module

---
+++ packages/nuxt/src/module.ts
@@ -0,0 +1,23 @@
+import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit';
+
+// Module options TypeScript interface definition
+
+ 
+export interface ModuleOptions {
+  pathToIcons?: string;
+}
+
+export default defineNuxtModule<ModuleOptions>({
+  meta: {
+    name: 'my-module',
+    configKey: 'myModule',
+  },
+  // Default configuration options of the Nuxt module
+  defaults: {},
+  setup() {
+    const resolver = createResolver(import.meta.url);
+
+    // Do not add the extension since the `.ts` will be transpiled to `.mjs` after `npm run prepack`
+    addPlugin(resolver.resolve('./runtime/plugin'));
+  },
+});

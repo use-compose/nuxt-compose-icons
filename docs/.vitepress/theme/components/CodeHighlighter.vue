5d58497e4a43731e7c4b72d4b64120d2341a1da2
Arthur Plazanet <plazanet.art@gmail.com>
2025-03-03 21:54:55 +0100

    Remove single vue-code-highlighter

--- docs/.vitepress/theme/components/CodeHighlighter.vue
+++ docs/.vitepress/theme/components/CodeHighlighter.vue
@@ -1,13 +1,12 @@
 <template>
   <VueCodeHighlighterMulti :code="code" />
-  <VueCodeHighlighter :code="'<p>test</p>'" lang="html" title="index.html" />
 </template>
 
 <script setup lang="ts">
 // import { VueCodeHighlighterMulti } from 'vue-code-highlighter';
 import * as vueCodeH from 'vue-code-highlighter';
 import 'vue-code-highlighter/dist/style.css'; // dont forget to import
-const { VueCodeHighlighterMulti, VueCodeHighlighter } = vueCodeH;
+const { VueCodeHighlighterMulti } = vueCodeH;
 interface CodeHighlighterProps {
   fileName?: string; // File name to display as metadata
   code: Array<{

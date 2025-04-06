5cd3175f6e884d42dcc92c9ada2cc6eccc86f516
arthur-plazanet <72502545+arthur-plazanet@users.noreply.github.com>
2025-04-06 21:55:42 +0200

    Update vercel_deploy_rules.sh

--- vercel_deploy_rules.sh
+++ vercel_deploy_rules.sh
@@ -2,7 +2,7 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_GIT_COMMIT_REF" =~ ^feature/.*$ ]]; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "staging" || "$VERCEL_GIT_COMMIT_REF" == "main" || "$VERCEL_GIT_COMMIT_REF" =~ ^feature/.*$ || "$VERCEL_GIT_COMMIT_REF" =~ ^renovate/.*$ ]]; then
   # Proceed with the build
   echo "✅ - Build can proceed"
   exit 1

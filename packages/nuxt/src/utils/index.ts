ea4c0458e3703eab098d2dc0f35d794326c21060
Arthur Plazanet <plazanet.art@gmail.com>
2025-10-27 20:18:21 +0100

    Fix unused files

--- packages/nuxt/src/utils/index.ts
+++ packages/nuxt/src/utils/index.ts
@@ -3,21 +3,23 @@
  */
 export { createComponentFromName } from './components/create-component-from-name';
 export { generateComponentName } from './components/generate-component-name';
+
 /*
  * Variable conventions utils
  */
 export { pascalCaseToKebabCase, toKebabCase, toPascalCase } from './convert-cases';
+
 /*
  * Filesystems utils
  */
 export { createComponentsDir } from './filesystem/create-components-dir';
 export { readDirectoryRecursively } from './filesystem/read-directory-recursively';
 export { writeComponentFile } from './filesystem/write-component-file';
+
 /*
  * SVGs utils
  */
-export { createSvgComponentCode } from './svg/create-svg-component.js';
-export { parseSvg } from './svg/parse-svg';
+export { optimizeSvg } from './svg/svgo-optimize';
 
 /*
  * CSS utils

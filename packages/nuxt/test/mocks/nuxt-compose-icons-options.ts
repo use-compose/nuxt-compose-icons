c77a1d14b0c1609bbe701d69b035cea9743aef14
Arthur Plazanet <plazanet.art@gmail.com>
2025-10-27 19:19:08 +0100

    Debug workflow 21

---
+++ packages/nuxt/test/mocks/nuxt-compose-icons-options.ts
@@ -0,0 +1,11 @@
+import type { NuxtComposeIconsOptions } from '~/src/module';
+
+export { moduleOptionsMock };
+
+const moduleOptionsMock: NuxtComposeIconsOptions = {
+  generatedComponentOptions: {
+    prefix: 'NuxtComposeIcon',
+    suffix: '',
+    case: 'pascal',
+  },
+};
